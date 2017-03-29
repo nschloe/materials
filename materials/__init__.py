198b5cc52d0e23d98d515021943a86779fbe7079
Nico Schlömer <nico.schloemer@gmail.com>
2017-03-29 10:23:50 +0200

    proper imports

--- materials/__init__.py
+++ materials/__init__.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 from . import air
+from . import argon
+from . import boron_trioxide
+from . import carbon_steel
+from . import copper
+from . import gallium_arsenide_liquid
+from . import gallium_arsenide_solid
+from . import graphite
+from . import silicon_carbide
+from . import silver
+from . import water
+
 
 from .__about__ import (
     __version__,
